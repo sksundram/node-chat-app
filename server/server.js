cb2f187614756e53376b2aa5ea5a62799b1c58f5
Shailesh Kumar <shailesh.sundram@gmail.com>
2018-04-29 13:22:52 +0530

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -21,23 +21,36 @@ io.on('connection', (socket) => {
     console.log('Client gets disconnected');
   });
 
+
   // Emit a 'newMessage' event, custom event -> when the server gets a new message
   // Emits an event to a single connection
-  // socket.emit('newMessage', {
-  //   from: 'Mike',
-  //   text: 'No. Not tonight.',
-  //   createdAt: 123456
-  // });
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+  });
+
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
 
   // Listen to a 'createMessage' event, custom event -> when the client sends a new message
   socket.on('createMessage', (message) => {
-
     // Emits an event to EVERY single connection
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
     });
+
+    // Emit to every client except the one emitting
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
   });
 });
 
